c9a692b2432f1a02f09cab5ed83d90d16f626fea
avitaliano <vitaliano@gmail.com>
2017-12-14 18:44:14 +0000

    remove hello example from package

--- R/build.R
+++ R/build.R
@@ -1,8 +1,3 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
 # You can learn more about package authoring with RStudio at:
 #
 #   http://r-pkgs.had.co.nz/
@@ -13,19 +8,13 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
-}
-
 # setup test
-#devtools::use_testthat()
+# devtools::use_testthat()
 devtools::use_package("snow")
 devtools::use_package("odbc")
 devtools::use_package("dplyr")
 
 # Google's R Style Guide
 # https://google.github.io/styleguide/Rguide.xml
-
 # checks code style
 # lintr::lint_package()
-#devtools::load_all()
--- tests/testthat/test-hello.R
+++
@@ -1,6 +0,0 @@
-library(datrProfile)
-context("Hello World")
-
-test_that("Hello, World!", {
-  expect_equal(hello(), "Hello, world!")
-})
